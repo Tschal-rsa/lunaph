85c67fe70358ff01ad6a54f955d2e873082b32d4
Tschal-rsa <lfz522@126.com>
2021-01-21 00:59:14 +0800

    0.2.1 Alpha

--- Lunaph.py
+++ Lunaph.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jan 17 22:00:36 2021
-
+ _                            _     
+| |   _   _ _ __   __ _ _ __ | |__  
+| |  | | | | '_ \ / _` | '_ \| '_ \ 
+| |__| |_| | | | | (_| | |_) | | | |
+|_____\__,_|_| |_|\__,_| .__/|_| |_|
+                       |_|          
 @author: lfz
 """
 
 import lunaph.lunaph
+import os
 
 if __name__ == '__main__':
+    os.chdir('lunaph')
     lunaph.lunaph.main()
